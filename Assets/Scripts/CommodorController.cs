4744d6003cd68362965b408329b2a2e232fd1f43
Valentin LABROUSSE <146397304+Rheaven-G@users.noreply.github.com>
2024-04-03 16:06:09 -0400

    Le Monsieur il bouge

--- Assets/Scripts/CommodorController.cs
+++ Assets/Scripts/CommodorController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.IMGUI.Controls;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.InputSystem.HID;
 using UnityEngine.UI;
@@ -9,20 +11,28 @@ using UnityEngine.UI;
 public class CommodorController : MonoBehaviour
 {
     // CONTANTS
-    public int GAMESTAGE = 0;
+    public int GAME_STAGE = 0;
     
     // COMPONENTS
+    // public GameManager _gameManager;
     public GameObject _startButton;
     public GameObject _actionMenu;
     public GameObject _genderAssignementSelection;
+    public GameObject _player;
     public List<Button> _actionButtons;
     
     // VARS
-    private bool _inMenu = true;
+    public bool isGenderMale;
+    // private bool _inMenu = false;
 
+    
+    //// PLAYER
+    // VARS
+    public float PIXEL_SPEED = 1f;
 
     private void Awake()
     {
+        _player.SetActive(false);
         _startButton.SetActive(true);
         _actionMenu.SetActive(false);
         _genderAssignementSelection.SetActive(false);
@@ -33,9 +43,15 @@ public class CommodorController : MonoBehaviour
         UpdateMenus();
     }
 
+    private void FixedUpdate()
+    {
+        UpdatePlayer();
+    }
+
+    // UPDATERS
     void UpdateMenus()
     {
-        switch (GAMESTAGE)
+        switch (GAME_STAGE)
         {
             case 0:
                 break;
@@ -43,22 +59,39 @@ public class CommodorController : MonoBehaviour
                 _genderAssignementSelection.SetActive(true);
                 Destroy(_startButton);
                 break;
-                
+            case 2:
+                _player.SetActive(true);
+                Destroy(_genderAssignementSelection);
+                break;
         }
     }
 
+    void UpdatePlayer()
+    {
+        float _hAxe = Input.GetAxis("Horizontal");
+        float _vAxe = Input.GetAxis("Vertical");
+
+        float xMove = _hAxe * PIXEL_SPEED;
+        float yMove = _vAxe * PIXEL_SPEED;
+
+        Vector3 move = new Vector3(xMove, yMove, 0);
+        _player.transform.position += move;
+    }
+    
+    // BUTTON ACTION CALLING
     void UpdateGAMESTAGE(int n = 1)
     {
         if (n == 1)
         {
-            GAMESTAGE++;
+            GAME_STAGE++;
         }
         else
         {
-            GAMESTAGE = n;
+            GAME_STAGE = n;
         }
     }
 
+    
     public void ButtonPressed(int buttonID)
     {
         switch (buttonID)
@@ -67,6 +100,20 @@ public class CommodorController : MonoBehaviour
                 UpdateGAMESTAGE();
                 break;
             case 1:
+                UpdateGAMESTAGE();
+                break;
+            
+            case 911:
+                //NORMAL
+                isGenderMale = true;
+                Debug.Log("PLAYER presses without hesitation");
+                UpdateGAMESTAGE();
+                break;
+            
+            case 912:
+                //HESITATE, THEN PRESS
+                isGenderMale = false;
+                Debug.Log("PLAYER hestiating but presses the Female Button");
                 UpdateGAMESTAGE();
                 break;
         }
