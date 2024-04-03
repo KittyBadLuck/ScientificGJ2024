f75f747437882aefc0422c64a026aa655cb25028
TIM <t.dekeyser@rubika-edu.ca>
2024-04-03 13:53:05 -0400

    camera control, camera lean, start menu ui

---
+++ Assets/Scripts/CameraAnimation.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mail;
+using UnityEngine;
+
+public class CameraAnimation : MonoBehaviour
+{
+    public Animator animator;
+
+    private bool Lean;
+    private bool Back;
+    
+    // Start is called before the first frame update
+    void Start()
+    { 
+        animator = gameObject.GetComponent<Animator>();
+        Lean = false;
+        Back = false;
+        Debug.Log(Lean);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E)) //maintenir E pour Zoomer puis relacher pour dezoomer
+        {
+            
+            animator.SetBool("Lean", true);
+            Debug.Log("Lean :" + Lean);
+            
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            animator.SetBool("Back", true);
+            animator.SetBool("Lean", false);
+        }
+        else
+        {
+            animator.SetBool("Back", false);
+            
+        }
+    }
+}
---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    //Variables
+    public Camera playerCamera;
+    public float lookSpeed = 2.0f;
+    public float lookXLimit = 45.0f;
+    float rotationX = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Lock cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+        rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+    }
+}
