1401fc85a23590cd86056cc6a446031f9fe8573e
Tim <t.dekeyser@rubika-edu.ca>
2024-04-04 19:27:00 -0400

    lean click

--- Assets/Scripts/CameraAnimation.cs
+++ Assets/Scripts/CameraAnimation.cs
@@ -2,42 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Net.Mail;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CameraAnimation : MonoBehaviour
 {
+    
+    
     public Animator animator;
-
-    private bool Lean;
+    
+    
+    
+    
+    
+    private bool isLean;
     
     
     // Start is called before the first frame update
     void Start()
     { 
-        animator = gameObject.GetComponent<Animator>();
-        Lean = false;
-        Debug.Log(Lean);
+        
+        isLean = false;
+        Debug.Log(isLean);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space)) //maintenir Space pour Zoomer puis relacher pour dezoomer
+        
+        if (Input.GetKeyDown(KeyCode.Space)) //maintenir Space pour Zoomer puis relacher pour dezoomer
         {
             
-            animator.SetBool("Lean", true);
-            Debug.Log("Lean :" + Lean);
+            if (isLean)
+            {
+                
+                isLean = false;
+                animator.SetBool("Lean", false);
+                animator.SetBool("Back", true);
+                Debug.Log("player back rn");
+                
+            } 
+            else
+            {
+
+                isLean = true;
+                animator.SetBool("Lean", true);
+                animator.SetBool("Back", false);
+                Debug.Log("player lean rn");
+            }
+            
             
         }
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            animator.SetBool("Back", true);
-            animator.SetBool("Lean", false);
-        }
-        else
-        {
-            animator.SetBool("Back", false);
-            
-        }
+        
+    }
+
+    public void StopBack()
+    {
+        animator.SetBool("Back", false);
     }
 }
